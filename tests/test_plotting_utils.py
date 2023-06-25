6a299a53103db13976b844410414791ad8df55a3
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2023-06-25 11:06:32 +0900

    refactor: Add None

--- tests/test_plotting_utils.py
+++ tests/test_plotting_utils.py
@@ -1,55 +1,55 @@
-def test_sum_annotation():
+def test_sum_annotation() -> None:
     pass
 
 
 # Actual plotting code
-def test_lineplot_arr():
+def test_lineplot_arr() -> None:
     pass
 
 
-def test_plot_arr():
+def test_plot_arr() -> None:
     pass
 
 
-def test_imshow_mask():
+def test_imshow_mask() -> None:
     pass
 
 
-def test_imshow_arr():
+def test_imshow_arr() -> None:
     pass
 
 
-def test_inset_cut_locator():
+def test_inset_cut_locator() -> None:
     pass
 
 
-def test_get_colorbars():
+def test_get_colorbars() -> None:
     pass
 
 
-def test_calculate_aspect_ratio():
+def test_calculate_aspect_ratio() -> None:
     pass
 
 
-def test_path_for_plot():
+def test_path_for_plot() -> None:
     pass
 
 
-def test_name_for_dim():
+def test_name_for_dim() -> None:
     pass
 
 
-def test_unit_for_dim():
+def test_unit_for_dim() -> None:
     pass
 
 
-def test_label_for_colorbar():
+def test_label_for_colorbar() -> None:
     pass
 
 
-def test_label_for_dim():
+def test_label_for_dim() -> None:
     pass
 
 
-def test_label_for_symmetry_point():
+def test_label_for_symmetry_point() -> None:
     pass
