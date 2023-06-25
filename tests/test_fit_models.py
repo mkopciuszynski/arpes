459e45b2c9305663f33560bf711167ec90122bcc
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2023-06-25 11:04:43 +0900

    refactor: add None. (This is a temporal treatment)

--- tests/test_fit_models.py
+++ tests/test_fit_models.py
@@ -1,37 +1,37 @@
-def test_xmodel_mixin():
+def test_xmodel_mixin() -> None:
     pass
 
 
-def test_xmodel_addition():
+def test_xmodel_addition() -> None:
     pass
 
 
-def test_xmodel_multiplication():
+def test_xmodel_multiplication() -> None:
     pass
 
 
-def test_xmodel_convolution():
+def test_xmodel_convolution() -> None:
     pass
 
 
-def test_step_convolution():
+def test_step_convolution() -> None:
     pass
 
 
 # Specific models
-def test_effective_mass_model():
+def test_effective_mass_model() -> None:
     pass
 
 
-def test_exponential_decay_c_model():
+def test_exponential_decay_c_model() -> None:
     pass
 
 
-def test_twoexponential_decay_c_model():
+def test_twoexponential_decay_c_model() -> None:
     pass
 
 
-def test_model_normalization():
+def test_model_normalization() -> None:
     """
     1. Cover normalization of standard peaks and peak shapes
     :return:
@@ -39,21 +39,21 @@ def test_model_normalization():
     pass
 
 
-def test_fermi_dirac_model():
+def test_fermi_dirac_model() -> None:
     pass
 
 
-def test_affine_broadened_fd():
+def test_affine_broadened_fd() -> None:
     pass
 
 
-def test_log_renormalization_model():
+def test_log_renormalization_model() -> None:
     pass
 
 
-def test_dirac_disperion_model():
+def test_dirac_disperion_model() -> None:
     pass
 
 
-def test_fermi_lorentzian_model():
+def test_fermi_lorentzian_model() -> None:
     pass
