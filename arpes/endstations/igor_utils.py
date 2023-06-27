32d720c11d8bd90e32ba7bce248b934644a922d1
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2023-06-27 11:33:51 +0900

    style(igor_utils.py): follow ruff warning

--- arpes/endstations/igor_utils.py
+++ arpes/endstations/igor_utils.py
@@ -1,6 +1,7 @@
 """Utilities related to handling Igor data ingestion quirks."""
 from __future__ import annotations
 
+import contextlib
 import itertools
 import os.path
 import subprocess
@@ -9,10 +10,11 @@ from typing import Any
 __all__ = ("shim_wave_note",)
 
 
-def shim_wave_note(path) -> dict[str, Any]:
+def shim_wave_note(path: str) -> dict[str, Any]:  # TODO(RA): should use Path
     """Hack to read the corrupted wavenote out of the h5 files that Igor has been producing.
 
-    h5 dump still produces the right value, so we use it from the command line in order to get the value of the note.
+    h5 dump still produces the right value, so we use it from the command line in order to get the
+    value of the note.
 
     This is not necessary unless you are trying to read HDF files exported from Igor (the
     preferred way before we developed an Igor data loading plugin).
@@ -24,7 +26,7 @@ def shim_wave_note(path) -> dict[str, Any]:
         The header/wavenote contents.
     """
     wave_name = os.path.splitext(os.path.basename(path))[0]
-    cmd = "h5dump -A --attribute /{}/IGORWaveNote {}".format(wave_name, path)
+    cmd = f"h5dump -A --attribute /{wave_name}/IGORWaveNote {path}"
     h5_out = subprocess.getoutput(cmd)
 
     split_data = h5_out[h5_out.index("DATA {") :]
@@ -32,11 +34,9 @@ def shim_wave_note(path) -> dict[str, Any]:
     data = split_data.split('"')[1]
 
     # remove stuff below the end of the header
-    try:
+    with contextlib.suppress(ValueError):
         data = data[: data.index("ENDHEADER")]
-    except ValueError:
-        pass
 
-    lines = [l.strip() for l in data.splitlines() if "=" in l]
-    lines = itertools.chain(*[l.split(",") for l in lines])
-    return dict([l.split("=") for l in lines])
+    lines = [line.strip() for line in data.splitlines() if "=" in line]
+    lines = itertools.chain(*[line.split(",") for line in lines])
+    return dict([line.split("=") for line in lines])
