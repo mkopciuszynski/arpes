4cd8918ffe3e799128fc0cbe11bb8be083b3c2f8
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2024-06-17 09:36:32 +0900

    ✨  Preparation for removing xr.Dataset.F (all method should be only in xr.DataArray.F)

--- tests/test_curve_fitting.py
+++ tests/test_curve_fitting.py
@@ -69,6 +69,15 @@ def test_broadcast_fitting(dataarray_cut: xr.DataArray) -> None:
         "a_lin_bkg",
         "a_offset",
     }
+    assert fit_results.results.F.parameter_names == {
+        "a_const_bkg",
+        "a_conv_width",
+        "a_center",
+        "a_width",
+        "a_lin_bkg",
+        "a_offset",
+    }
+
     assert fit_results.results.F.band_names == {"a_"}
     assert fit_results.F.broadcast_dimensions == ["phi"]
     assert fit_results.F.fit_dimensions == ["eV"]
@@ -87,6 +96,22 @@ def test_broadcast_fitting(dataarray_cut: xr.DataArray) -> None:
             ],
         ),
     )
+    np.testing.assert_almost_equal(
+        fit_results.results.F.mean_square_error().values,
+        np.array(
+            [
+                1558314.89601851,
+                1511866.71409967,
+                1458591.79853167,
+                457007.03427883,
+                1279915.83565262,
+                1319729.87019014,
+                1229990.43899916,
+                1193485.20821447,
+            ],
+        ),
+    )
+
     np.testing.assert_allclose(
         fit_results.F.s("a_conv_width").values,
         np.array(
@@ -102,6 +127,22 @@ def test_broadcast_fitting(dataarray_cut: xr.DataArray) -> None:
             ],
         ),
     )
+    np.testing.assert_allclose(
+        fit_results.results.F.s("a_conv_width").values,
+        np.array(
+            [
+                1.54832468e03,
+                1.36921697e06,
+                5.23737413e06,
+                1.60255187e01,
+                1.16205297e07,
+                8.98171495e06,
+                3.16863545e06,
+                3.57829642e06,
+            ],
+        ),
+    )
+
     params_ = fit_results.results.F.param_as_dataset("a_conv_width")
     np.testing.assert_almost_equal(
         params_["value"].values,
