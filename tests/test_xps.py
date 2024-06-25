31a58430e83b5ef8b17112edab023a9bb561a355
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2024-06-25 10:37:52 +0900

    ✅  Add test for xps.py

---
+++ tests/test_xps.py
@@ -0,0 +1,20 @@
+"""Unit test for xps.py."""
+
+import numpy as np
+import xarray as xr
+from arpes.analysis.xps import approximate_core_levels
+
+
+class TestXPS:
+    """Test class for xps analysis.
+
+    While the test data is not XPS, but it's not a problem to test.
+    """
+
+    def test_approximate_core_levels(self, dataarray_cut2: xr.DataArray) -> None:
+        """Test for approximate_core_levels."""
+        energies = approximate_core_levels(dataarray_cut2.S.sum_other(["eV"]))
+        np.testing.assert_array_almost_equal(
+            energies,
+            [9.36826347305, 9.58383233535, 9.89520958085],
+        )
