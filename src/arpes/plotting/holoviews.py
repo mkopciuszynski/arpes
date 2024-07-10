8fcf211ef6c680c15a5e876e16a58acb104c59ad
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2024-07-10 10:02:39 +0900

    🐛  Fix bug to show the kspace data.
    it may originate from the holoviews bug, which I cannot handle.

--- src/arpes/plotting/holoviews.py
+++ src/arpes/plotting/holoviews.py
@@ -2,16 +2,15 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, TypedDict, Unpack
+from typing import TypedDict, Unpack
 
 import holoviews as hv
 import numpy as np
+import xarray as xr
 from holoviews import DynamicMap, Image
 
 from arpes.constants import TWO_DIMENSION
-
-if TYPE_CHECKING:
-    import xarray as xr
+from arpes.utilities.normalize import normalize_to_spectrum
 
 hv.extension("bokeh")
 
@@ -29,20 +28,31 @@ class CrosshairViewParam(TypedDict):
 def crosshair_view(dataarray: xr.DataArray, **kwargs: Unpack[CrosshairViewParam]) -> None:
     """Show Crosshair view.
 
-    2024/07/08: On Jupyterlab on safari, it may not work correctly.
+    Todo:
+    There are some issues.
+
+    * 2024/07/08: On Jupyterlab on safari, it may not work correctly.
+    * 2024/07/09: Some xr.DataArray data (especially the output of the convert_to_kspace() cannot be
+      handled correctly.  (https://github.com/holoviz/holoviews/issues/6317)
+    * 2024/07/10: Incompatibility between bokeh and matplotlib about which is "x-" axis about
+      plotting xarray data.
+
     """
     kwargs.setdefault("width", 300)
     kwargs.setdefault("height", 300)
-    kwargs.setdefault("cmap", "virids")
+    kwargs.setdefault("cmap", "viridis")
     kwargs.setdefault("log", False)
     kwargs.setdefault("profile_view_height", 100)
 
     assert dataarray.ndim == TWO_DIMENSION
     max_coords = dataarray.G.argmax_coords()
-    posx = hv.streams.PointerX(x=0)
-    posy = hv.streams.PointerY(y=0)
+    posx = hv.streams.PointerX(x=max_coords[dataarray.dims[0]])
+    posy = hv.streams.PointerY(y=max_coords[dataarray.dims[1]])
 
     second_weakest_intensity = np.partition(np.unique(dataarray.values.flatten()), 1)[1]
+    dataarray = (
+        dataarray if isinstance(dataarray, xr.DataArray) else normalize_to_spectrum(dataarray)
+    )
     plot_lim: tuple[None | np.float64, np.float64] = (
         (second_weakest_intensity * 0.1, dataarray.max().item() * 10)
         if kwargs["log"]
@@ -56,7 +66,16 @@ def crosshair_view(dataarray: xr.DataArray, **kwargs: Unpack[CrosshairViewParam]
         lambda y: hv.HLine(y=y or max_coords[dataarray.dims[1]]),
         streams=[posy],
     )
-    img: Image = hv.Image(dataarray, kdims=list(dataarray.dims)).opts(
+    # Memo: (ad-hoc fix) to avoid the problem concerning https://github.com/holoviz/holoviews/issues/6317
+    img: Image = hv.Image(
+        (
+            dataarray.coords[dataarray.dims[1]].values,
+            dataarray.coords[dataarray.dims[0]].values,
+            dataarray.values,
+        ),
+        kdims=list(dataarray.dims),
+        vdims=["spectrum"],
+    ).opts(
         width=kwargs["width"],
         height=kwargs["height"],
         logz=kwargs["log"],
