2a1dc06158725320480e48b79dea157db95b695b
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2024-07-12 10:34:12 +0900

    🎉  Release 4.0

--- src/arpes/__init__.py
+++ src/arpes/__init__.py
@@ -9,7 +9,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ._typing import ConfigSettings, ConfigType
 # Use both version conventions for people's sanity.
-VERSION = "4.0.0 beta2"
+VERSION = "4.0.0"
 __version__ = VERSION
 
 
